038fb12d5886ba128278332ba01e3ff92a995ef0
kronicmage <kronicmage@gmail.com>
2016-12-09 13:26:06 -0500

    Minor theming adjustments

--- app/src/main/java/com/zergtel/ztvdc/MainActivity.java
+++ app/src/main/java/com/zergtel/ztvdc/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Lol this button doesn't work", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -87,10 +87,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
